df98846532d49e9253de979a22444bfa0c149cef
Mike Turner <miketurnerjr78@gmail.com>
2019-09-23 15:05:06 -0500

    got user logi working

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -15,8 +15,11 @@ class UserTableSeeder extends Seeder
         DB::table('users')->insert([
             'first_name' => 'Mike',
             'last_name' => 'Turner',
-            'email' => Str::random(10).'@gmail.com',
+            'email' => 'miketurnerjr78@gmail.com',
             'password' => bcrypt('password'),
+            'created_at' => now(),
+            'updated_at' => now(),
+            'email_verified_at' => now()
         ]);
     }
 }
